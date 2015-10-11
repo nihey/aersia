1fc84d42f8064f9a4df6415f965a1134612d753d
Nihey Takizawa <nihey.takizawa@gmail.com>
2015-10-11 19:47:41 -0300

    Add history forwarding/rewinding and randomizing controls
    Also auto-randomize when no music file was found

--- scripts/index.jsx
+++ scripts/index.jsx
@@ -5,20 +5,43 @@ import xhr from 'xhr';
 import parse from 'parse';
 
 class Index extends React.Component {
-  select(index=0) {
+  select(index=0, advance=true) {
     this.state.audio && this.state.audio.pause();
 
     let track = this.state.tracks[index];
     let audio = new Audio(track.location);
-    this.setState({audio, index, playing: false, disabled: true}, function() {
+    let historyIndex = this.state.historyIndex;
+    if (advance) {
+      historyIndex++;
+      this.state.history.splice(historyIndex, 0, index);
+    }
+
+    this.setState({
+      audio,
+      playing: false,
+      disabled: true,
+      index,
+      historyIndex,
+      history: this.state.history,
+    }, function() {
       let offset = this.refs.controlBar.clientHeight;
       let scrollY = this.refs['track_' + index].getBoundingClientRect().top;
       window.scrollBy(0, scrollY - offset);
 
+      // Update React's playing status when the audio fire these events
       audio.addEventListener('playing', () => this.setState({playing: true, disabled: false}));
       audio.addEventListener('pause', () => this.setState({playing: false}));
       audio.addEventListener('ended', () => {
         this.setState({playing: false}, () => {this.randomize()});
+      });
+
+      // If there was an error, remove the current index and randomize
+      audio.addEventListener('error', () => {
+        this.state.history.splice(historyIndex, 1)
+        this.setState({
+          historyIndex: historyIndex - 1,
+          history: this.state.history,
+        }, this.randomize, this);
       });
       audio.volume = 0.5;
       audio.play();
@@ -40,6 +63,26 @@ class Index extends React.Component {
     }
   }
 
+  onStep(offset) {
+    return () => {
+      // Don't allow any negative index
+      let index = this.state.historyIndex + offset;
+      index = index < 0 ? 0 : index;
+
+      // If no track was found on the given index randomize
+      let track = this.state.history[index];
+      if (track === undefined) {
+        this.randomize();
+        return;
+      }
+
+      // If a track was found, select it
+      this.setState({historyIndex: index, disabled: true}, function() {
+        this.select(track, false);
+      }, this);
+    }
+  }
+
   togglePlay() {
     let playing = this.state.playing;
     playing && this.state.audio.pause();
@@ -54,8 +97,11 @@ class Index extends React.Component {
     super(props);
     this.state = {
       tracks: [],
+      history: [],
       audio: null,
       index: null,
+      historyIndex: -1,
+      disabled: true,
     };
   }
 
@@ -74,9 +120,15 @@ class Index extends React.Component {
         <button className="square" onClick={this.togglePlay.bind(this)} disabled={this.state.disabled}>
           <i className={'fa ' + (this.state.playing ? 'fa-pause' : 'fa-play')}/>
         </button>
-        <button className="square"><i className="fa fa-step-backward"/></button>
-        <button className="square"><i className="fa fa-step-forward"/></button>
-        <button className="square"><i className="fa fa-random"/></button>
+        <button className="square" onClick={this.onStep(-1)} disabled={this.state.disabled}>
+          <i className="fa fa-step-backward"/>
+        </button>
+        <button className="square" onClick={this.onStep(1)} disabled={this.state.disabled}>
+          <i className="fa fa-step-forward"/>
+        </button>
+        <button className="square" onClick={this.randomize.bind(this)} disabled={this.state.disabled}>
+          <i className="fa fa-random"/>
+        </button>
         <div className="volume"></div>
         <div className="progress">
           <span>00:00</span>
